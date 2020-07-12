d80b4842317f25614b70776dc9b627de22d12655
Patrik Jankuv <jankupat@fel.cvut.cz>
2020-07-12 21:50:49 +0200

    use data binding for getting dice image

--- app/src/main/java/com/example/kotlin_dice_roller/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_dice_roller/MainActivity.kt
@@ -3,31 +3,38 @@ package com.example.kotlin_dice_roller
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.kotlin_dice_roller.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var diceImage: ImageView
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         //remove top bar from the screen
         if (supportActionBar != null)
             supportActionBar?.hide()
 
 
-        diceImage = findViewById(R.id.dice_image)
+        diceImage = binding.diceImage
+
+//        diceImage = findViewById(R.id.dice_image)
         diceImage.setOnClickListener {
             rollDice()
         }
     }
 
     private fun rollDice() {
-        val randomInt = Random().nextInt(6) + 1
 
-        val drawableResource = when (randomInt) {
+        val drawableResource = when (Random().nextInt(6) + 1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
